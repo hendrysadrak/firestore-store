170b0fe0efc3eb84fd70c1052563699333e1edec
Hendry Sadrak <hendry.sadrak@outlook.com>
2018-05-29 12:00:51 +0300

    add tests

---
+++ test/store.js
@@ -0,0 +1,101 @@
+import test from 'ava';
+import session from 'express-session';
+import firestoreStore from '../lib/firestore-store';
+import path from 'path';
+
+// crate firebase session
+
+const admin = require( 'firebase-admin' );
+
+const firebase = admin.initializeApp( {
+	credential:  admin.credential.cert( path.join( __dirname, 'testServiceAccountCredentials.json' ) ),
+	databaseURL: 'https://firestore-store-tests.firebaseio.com'
+} );
+
+const database = firebase.firestore();
+
+// create session store
+
+const FirestoreStore = firestoreStore( session );
+
+const store = new FirestoreStore( {
+	database: database,
+} );
+
+// pre data
+
+const sessName = `ses${Date.now()}`;
+const sessVal  = { thisissession: true, date: Date.now(), random: Math.random() };
+
+// tests
+
+test.serial.cb( 'store clear before', t => {
+	store.clear( ( err ) => {
+		t.is( err, null );
+
+		t.end();
+	} )
+} );
+
+test.serial.cb( 'store get, set, touch', t => {
+	store.set( sessName, sessVal, ( err ) => {
+		t.is( err, null );
+
+		store.get( sessName, ( err, val ) => {
+			t.is( err, null );
+			t.deepEqual( val, sessVal );
+
+			store.touch( sessName, sessVal, ( err ) => {
+				t.is( err, null );
+				t.end();
+			} );
+
+		} );
+
+	} );
+} );
+
+test.serial.cb( 'store length before clear', t => {
+	store.length( ( err, len ) => {
+		t.is( err, null );
+		t.is( len, 1 );
+
+		t.end();
+	} )
+} );
+
+test.serial.cb( 'store clear', t => {
+	store.clear( ( err ) => {
+		t.is( err, null );
+
+		t.end();
+	} )
+} );
+
+test.serial.cb( 'store length after clear', t => {
+	store.length( ( err, len ) => {
+		t.is( err, null );
+		t.is( len, 0 );
+
+		t.end();
+	} )
+} );
+
+test.serial.cb( 'store destory', t => {
+	store.set( sessName, sessVal, ( err ) => {
+		t.is( err, null );
+
+		store.destroy( sessName, ( err ) => {
+			t.is( err, null );
+
+			store.length( ( err, len ) => {
+				t.is( err, null );
+				t.is( len, 0 );
+
+				t.end();
+			} )
+
+		} );
+
+	} );
+} );
